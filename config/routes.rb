3aebb202ebce1408e0e70428d6539ee6fa9d6fac
Brianna Forster <brianna.forster@gmail.com>
2017-08-21 21:22:08 -0700

    Generate resources for microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :microposts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "application#hello"
+  root 'users#index'
 end
