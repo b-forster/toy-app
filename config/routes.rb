348a1cacbea8a9a0454fc159c50dcc8cdc019e89
Brianna Forster <brianna.forster@gmail.com>
2017-08-21 21:31:16 -0700

    Set up validations and associations for User and Micropost models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :microposts
-  end
+  resources :users
+  resources :microposts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'users#index'
